b0e7e893779411a0fc15343dff9c8bd6a7473df3
srihan93 <srihan93@gmail.com>
2021-09-26 14:19:22 +0530

    added requried models attribute, workflow, reason, status and reasoninstatus

---
+++ ServiceFlow.Webhost/Controllers/StatusController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceFlow.Webhost.Controllers
+{
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpPost]
+        [RouteAttribute("api/status")]
+        public IActionResult AddStatus()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [RouteAttribute("api/reason")]
+        public IActionResult AddReason()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [RouteAttribute("api/status/reason")]
+        public IActionResult MapReasonToStatus()
+        {
+            return null;
+        }
+    }
+}
