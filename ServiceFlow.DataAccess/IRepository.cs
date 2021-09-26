36e263d6ab7add5e26c8baa9f365f9e278605296
srihan93 <srihan93@gmail.com>
2021-09-26 18:00:48 +0530

    added repository interface and logics

---
+++ ServiceFlow.DataAccess/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFlow.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+
+        T Get(Expression<Func<T, bool>> expression);
+
+        IQueryable<T> GetMany(Expression<Func<T, bool>> expression);
+
+        bool Insert(T obj);
+
+        bool Update(T obj);
+
+        bool Delete(int id);
+
+        int Count();
+
+        void Save();
+    }
+}
--- ServiceFlow.DataAccess/ServiceFlowSqlRepository.cs
+++ ServiceFlow.DataAccess/ServiceFlowSqlRepository.cs
@@ -1,8 +1,64 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceFlow.DataAccess.AppDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace ServiceFlow.DataAccess
 {
-    public class ServiceFlowSqlRepository
+    public class ServiceFlowSqlRepository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext _dbContext;
+
+        public ServiceFlowSqlRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetMany(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
