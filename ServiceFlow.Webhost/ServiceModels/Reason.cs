3fb0bf787802f0327c31c240981cf83feb625f9e
srihan93 <srihan93@gmail.com>
2021-10-07 22:00:31 +0530

    added reason

--- ServiceFlow.Webhost/ServiceModels/Reason.cs
+++ ServiceFlow.Webhost/ServiceModels/Reason.cs
@@ -7,5 +7,8 @@ namespace ServiceFlow.Webhost.ServiceModels
 {
     public class Reason
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
     }
 }
